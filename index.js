fb7f7a27da4fe377c2a0d4ff9a4a9ae400f1c8f1
Naresh Challa <124749877+Naresh2219@users.noreply.github.com>
2024-04-13 22:10:27 +0530

    Add fade-in animation to image and sections; update CSS animations and keyframes; modify HTML structure for consistency
    The changes include:
    
    * Adding a fade-in animation to the home image and about section by using CSS key

--- index.js
+++ index.js
@@ -6,24 +6,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
     });
   });
 });
-
-document.addEventListener('DOMContentLoaded',function(){
-  var Section = document.getElementById('fadeInselection');
-
-  function isInViewport(element){
-    var rect = element.getBoundingClientRect();
-    return (
-      rect.top >=0 &&
-      rect.left >0 &&
-      rect.bottom <=(window.innerHeight ||document.documentElement.clientHeight) &&
-      rect.right <=(window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-  function fadeInSection(){
-    if(isInViewport(Section)){
-      Section.classList.add('visible');
-      //window.removeEventListener('scroll', fadeInSection);
-    }
-  }
-  window.addEventListener('scroll', fadeInSection)
-});
