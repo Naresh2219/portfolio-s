5c710afce5049c2d2e9c8028a876d4c1e81bb40f
Naresh Challa <124749877+Naresh2219@users.noreply.github.com>
2024-04-14 16:06:51 +0530

    "Added WOW.js for scroll animation and removed unused fade-in code"
    This commit includes the following changes:
    
    * Added the WOW.js library to enable scroll animation effects.
    * Removed the

--- index.js
+++ index.js
@@ -8,27 +8,29 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
-
-  // Fade-in effect on scroll
-  var fadeInElements = document.querySelectorAll('.fade-in');
-  
-  function fadeInOnScroll() {
-    fadeInElements.forEach(function(element) {
-      if (isElementInViewport(element)) {
-        element.style.opacity = "1";
-      }
-    });
-  }
-  
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-  
-  window.addEventListener('scroll', fadeInOnScroll);
+  new WOW().init();
 });
+
+//   // Fade-in effect on scroll
+//   var fadeInElements = document.querySelectorAll('.fade-in');
+  
+//   function fadeInOnScroll() {
+//     fadeInElements.forEach(function(element) {
+//       if (isElementInViewport(element)) {
+//         element.style.opacity = "1";
+//       }
+//     });
+//   }
+  
+//   function isElementInViewport(el) {
+//     var rect = el.getBoundingClientRect();
+//     return (
+//       rect.top >= 0 &&
+//       rect.left >= 0 &&
+//       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+//   }
+  
+//   window.addEventListener('scroll', fadeInOnScroll);
+// });
